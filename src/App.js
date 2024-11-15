16ad8133a72b7ea789a2e52790a707366bdd8922
Barnali29 <barnali03b@gmail.com>
2024-11-15 17:52:36 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useEffect, useState } from 'react';
 import { Todocontextprovider } from './Context';
 import Todoform from './Component/Todoform';
 import Todoitem from './Component/Todoitem';
+
 function App() {
   const[todos,Settodos]=useState([])
 
@@ -36,6 +37,7 @@ useEffect(()=>{
   <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Tasks</h1>
+                   
                     <div className="mb-4"> 
                         <Todoform />
                     </div>
